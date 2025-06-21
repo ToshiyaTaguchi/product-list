1dc849496aca75383b202725c667319a7f7f1f69
toshiya <toshiyam171054@ymail.ne.jp>
2025-06-21 14:43:21 +0900

    ダミーデータの追加

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,16 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductsTableSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+    public function run(): void
+{
+    $this->call([
+        ProductsTableSeeder::class,
+    ]);
+}
 }
